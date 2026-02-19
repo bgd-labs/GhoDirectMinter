6fac37f5b4d33829a7f188aff009f0fc9c61d941
Lukas <lukasstrassel@googlemail.com>
2026-02-19 15:23:00 +0100

    fix: lint code

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 import {EthereumScript} from "solidity-utils/contracts/utils/ScriptUtils.sol";
 import {IPoolAddressesProvider} from "aave-v3-origin/contracts/interfaces/IPoolAddressesProvider.sol";
 import {ICollector} from "aave-v3-origin/contracts/treasury/ICollector.sol";
-import {ITransparentProxyFactory} from
-  "solidity-utils/contracts/transparent-proxy/interfaces/ITransparentProxyFactory.sol";
+import {
+  ITransparentProxyFactory
+} from "solidity-utils/contracts/transparent-proxy/interfaces/ITransparentProxyFactory.sol";
 import {GhoDirectMinter} from "../src/GhoDirectMinter.sol";
 import {IGhoToken} from "../src/interfaces/IGhoToken.sol";
 
@@ -34,11 +35,14 @@ library DeploymentLibrary {
   function _deployCore() internal returns (address) {
     address council = 0x8513e6F37dBc52De87b166980Fa3F50639694B60;
 
-    return ITransparentProxyFactory(MiscEthereum.TRANSPARENT_PROXY_FACTORY).create(
-      0xE4C958dE49303c9be571E00582CF9454586dE76F,
-      GovernanceV3Ethereum.EXECUTOR_LVL_1,
-      abi.encodeWithSelector(GhoDirectMinter.initialize.selector, address(GovernanceV3Ethereum.EXECUTOR_LVL_1), council)
-    );
+    return ITransparentProxyFactory(MiscEthereum.TRANSPARENT_PROXY_FACTORY)
+      .create(
+        0xE4C958dE49303c9be571E00582CF9454586dE76F,
+        GovernanceV3Ethereum.EXECUTOR_LVL_1,
+        abi.encodeWithSelector(
+          GhoDirectMinter.initialize.selector, address(GovernanceV3Ethereum.EXECUTOR_LVL_1), council
+        )
+      );
   }
 
   // function _deployLido() internal returns (address) {
